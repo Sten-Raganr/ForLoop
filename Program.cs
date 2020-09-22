e09c5a8271a0a10e63b7279cd1a68aa5b069df8a
Opilane TTHK <opilane@tthk.ee>
2020-09-22 13:25:42 +0300

    first commit git branch -M master

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 14; i++)
+            {
+                Console.WriteLine($"{i+1}.I will not skateboard in the halls.");
+            }
+
+            
+        }
+    }
+}
